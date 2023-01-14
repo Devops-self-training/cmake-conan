341059d9e5017154c7d3c956d0aebc20da7b8d57
Hoang Le <le.dinhhoang.1207@outlook.com>
2023-01-14 06:02:46 +0000

    update code

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspaces/hello-world/test-project
-# Build directory: /workspaces/hello-world/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
